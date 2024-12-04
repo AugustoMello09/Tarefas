ab2bfac3680ec5fabf041f347968d1c6d72ab997
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-12-04 15:25:28 -0300

    feat : entity Tarefa

---
+++ tarefasBackend/src/main/java/io/gitHub/AugustoMello09/tarefas/domain/entities/Tarefa.java
@@ -0,0 +1,42 @@
+package io.gitHub.AugustoMello09.tarefas.domain.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_tarefa")
+public class Tarefa implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = true, unique = true)
+	private String name;
+
+	private BigDecimal cost;
+
+	private LocalDate dueDate;
+
+	private Integer position;
+
+}
