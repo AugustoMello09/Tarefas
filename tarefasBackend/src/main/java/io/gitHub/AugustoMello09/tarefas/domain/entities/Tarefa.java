bafed7e4ae52412d8392bae06d40f621e3396327
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-12-06 14:38:25 -0300

    feat: add JPA relationships for Usuario entity, Tarefa Entity and Cargo Entity

--- tarefasBackend/src/main/java/io/gitHub/AugustoMello09/tarefas/domain/entities/Tarefa.java
+++ tarefasBackend/src/main/java/io/gitHub/AugustoMello09/tarefas/domain/entities/Tarefa.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -36,7 +38,11 @@ public class Tarefa implements Serializable {
 	private BigDecimal cost;
 
 	private LocalDate dueDate;
-
+	
 	private Integer position;
+	
+	@ManyToOne
+	@JoinColumn(name = "usuario_id")
+	private Usuario usuario;
 
 }
--- tarefasBackend/src/main/java/io/gitHub/AugustoMello09/tarefas/domain/entities/Usuario.java
+++ tarefasBackend/src/main/java/io/gitHub/AugustoMello09/tarefas/domain/entities/Usuario.java
@@ -1,24 +1,32 @@
 package io.gitHub.AugustoMello09.tarefas.domain.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 @Getter
 @Setter
@@ -38,5 +46,20 @@ public class Usuario implements Serializable {
 	
 	@JsonIgnore
 	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(name = "tb_usuario_cargo", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "cargo_id"))
+	private Set<Cargo> cargos = new HashSet<>();
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Tarefa> tarefas = new ArrayList<>();
 
+	public Usuario(UUID id, String name, String email, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+	
 }
