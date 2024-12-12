b62e3dd0bd8f1e721662666298c3680b3790f70b
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-12-12 14:32:04 -0300

    feat : inicio created

--- tarefasFrontend/tailwind.config.js
+++ tarefasFrontend/tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'dm': ['DM Sans', 'sans-serif'],
+        'Gro' : ['Space Grotesk', 'sans-serif']
+      },
+      screens: {
+        'auto': {'max' : '1024px'},
+        'menor': {'max' : '824px'},
+        'xr': {'max' : '599px'},
+        'xd': {'max' : '409px'},
+      }
+    },
   },
   plugins: [],
 }
