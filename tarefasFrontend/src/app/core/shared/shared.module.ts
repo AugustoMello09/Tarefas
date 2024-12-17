e080df8cefa7fa86f68888b3fa7ff0918f2ba3f3
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-12-17 10:12:01 -0300

    feat : add footer One

--- tarefasFrontend/src/app/core/shared/shared.module.ts
+++ tarefasFrontend/src/app/core/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterOneComponent } from './layout/footer-one/footer-one.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterOneComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    FooterOneComponent
   ]
 })
 export class SharedModule { }
