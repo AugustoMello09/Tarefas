820bd6777c41a70b7ad5385fd44b169967dd1cfe
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2025-01-08 15:46:16 -0300

    feat : Add Dockerfile tarefasBackend

--- tarefasBackend/src/main/resources/db/migration/V3__Seed-user.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO tb_usuario (id, name, email, password, notification) VALUES (UUID(), 'Augusto', 'jose@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', false);
-INSERT INTO tb_usuario_cargo (usuario_id, cargo_id) SELECT id, 2 FROM tb_usuario WHERE email = 'jose@gmail.com';
--- tarefasBackend/src/main/resources/db/migration/V4__Seed-tarefa.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO tb_tarefa (name, cost, due_date, position, favorite) VALUES ('Teste prático - lista de tarefas', 0, '2024-11-30', 0, false);
-
-UPDATE tb_tarefa SET usuario_id = (SELECT id FROM tb_usuario WHERE email = 'jose@gmail.com') WHERE id = 1;
